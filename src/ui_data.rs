46b823938faa58fc47efb1f4e94c7a573491c202
peacess <peacess@163.com>
2023-08-31 17:49:57 +0800

    collapse button

--- src/ui_data.rs
+++ src/ui_data.rs
@@ -1,31 +1,60 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts};
 
-#[derive(Default, Resource)]
+#[derive(Resource)]
 pub struct AppUi {
-    left_id: f32,
-    right_id: f32,
+    collapse: bool,
 }
 
+impl Default for AppUi {
+    fn default() -> Self {
+        Self {
+            collapse: true,
+        }
+    }
+}
+
+
 impl AppUi {
+    fn collapse_str(&self) -> &'static str {
+        match self.collapse {
+            true => "<",
+            false => ">"
+        }
+    }
     pub fn update(
         mut app_ui: ResMut<AppUi>,
         mut contexts: EguiContexts,
     ) {
         let ctx = contexts.ctx_mut();
-        app_ui.left_id = egui::SidePanel::left("left_panel")
-            .resizable(true)
+        if cfg!(debug_assertions) {
+            ctx.set_debug_on_hover(true);
+        } else {
+            ctx.set_debug_on_hover(false);
+        }
+
+        if !app_ui.collapse {
+            egui::SidePanel::right("left_panel")
+                .min_width(0.0)
+                .resizable(true)
+                .show(ctx, |ui| {
+                    ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+                })
+                .response
+                .rect
+                .width();
+        }
+        egui::CentralPanel::default()
             .show(ctx, |ui| {
-                ui.label("Left");
-                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
-            })
-            .response
-            .rect
-            .width();
-        app_ui.right_id = egui::CentralPanel::default()
-            .show(ctx, |ui| {
-                ui.label("Right");
-                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+                let re = {
+                    let mut left_center = ui.min_rect().center();
+                    left_center.x = ui.min_rect().right() - 15.0;
+                    egui::Rect::from_center_size(left_center, egui::Vec2::splat(30.0))
+                };
+                let button = ui.put(re, egui::Button::new(app_ui.collapse_str()).small());
+                if button.clicked() {
+                    app_ui.collapse = !app_ui.collapse;
+                }
             })
             .response
             .rect
