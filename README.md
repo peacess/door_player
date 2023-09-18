d5abb7921a59fac14e06932b61f0310351f89e07
peacess <peacess@163.com>
2023-09-18 19:37:28 +0800

    play next/pre file

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # player
 dependencies: bevy, egui, ffmpeg  
 
+# build
+[ffmpeg](https://github.com/zmwangx/rust-ffmpeg/wiki/Notes-on-building)
+[sdl2](https://github.com/Rust-SDL2/rust-sdl2)
+
 # see
 [egui-video(player)](https://github.com/n00kii/egui-video)  
 [ffplay源码分析4-音视频同步](https://www.cnblogs.com/leisure_chn/p/10307089.html)
--- src/ui_data.rs
+++ src/ui_data.rs
@@ -1,4 +1,9 @@
-use crate::{AudioDevice, init_audio_device_default, Player};
+use std::{fs, path};
+
+use egui::{Event, Key, Ui};
+
+use crate::{AudioDevice, init_audio_device_default, Player, PlayerState};
+use crate::player::Streamer;
 
 pub struct AppUi {
     collapse: bool,
@@ -6,8 +11,126 @@ pub struct AppUi {
     player: Option<Player>,
 
     media_path: String,
+    no_scale: bool,
     // stream_size_scale: f32,
     // seek_frac: f32,
+}
+
+impl AppUi {
+    pub(crate) fn handle_key_player(ui: &mut Ui, player: &mut Player) {
+        ui.input(|k| {
+            for e in &k.events {
+                let mut seek = 0.0f32;
+                match e {
+                    Event::Key { key, pressed: true, .. } => {
+                        match *key {
+                            Key::ArrowLeft => {
+                                let mut v = player.video_streamer.lock();
+                                let els = v.elapsed_ms().get() as f32 - 1.0;
+                                if els > 0.0 {
+                                    seek = els / v.duration_ms() as f32;
+                                }
+                            }
+                            Key::ArrowRight => {
+                                let mut v = player.video_streamer.lock();
+                                let els = v.elapsed_ms().get() as f32 + 1.0;
+                                seek = els / v.duration_ms() as f32;
+                            }
+                            Key::ArrowUp => {}
+                            Key::ArrowDown => {}
+                            Key::Space => {
+                                let state = player.player_state.get();
+                                match state {
+                                    PlayerState::Paused | PlayerState::Stopped => {
+                                        player.resume();
+                                    }
+                                    PlayerState::Playing => {
+                                        player.pause();
+                                    }
+                                    _ => {}
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+                if seek > 0.0 {
+                    player.seek(seek);
+                }
+            }
+        });
+    }
+
+    pub(crate) fn next_file(file: &str) -> String {
+        let path_file = path::PathBuf::from(file);
+        if path_file.file_name().is_none() {
+            return String::default();
+        }
+        let mut files = Vec::new();
+        match fs::read_dir(path_file.parent().unwrap()) {
+            Err(e) => {
+                log::error!("{}",e);
+                return String::default();
+            }
+            Ok(read_dir) => {
+                for f in read_dir {
+                    if let Ok(ff) = f {
+                        files.push(ff.file_name());
+                    }
+                }
+            }
+        }
+
+        files.sort();
+        if let Ok(i) = files.binary_search(&path_file.file_name().unwrap().to_os_string()) {
+            let i = {
+                if i == files.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            };
+            let f = files.get(i);
+            return path_file.parent().unwrap().join(f.unwrap()).to_string_lossy().to_string();
+        }
+        return String::default();
+    }
+
+    pub(crate) fn pre_file(file: &str) -> String {
+        let path_file = path::PathBuf::from(file);
+        if path_file.file_name().is_none() {
+            return String::default();
+        }
+        let mut files = Vec::new();
+        match fs::read_dir(path_file.parent().unwrap()) {
+            Err(e) => {
+                log::error!("{}",e);
+                return String::default();
+            }
+            Ok(read_dir) => {
+                for f in read_dir {
+                    if let Ok(ff) = f {
+                        files.push(ff.file_name());
+                    }
+                }
+            }
+        }
+
+        files.sort();
+        if let Ok(i) = files.binary_search(&path_file.file_name().unwrap().to_os_string()) {
+            let i = {
+                if i == 0 {
+                    files.len() - 1
+                } else {
+                    i - 1
+                }
+            };
+            let f = files.get(i);
+            return path_file.parent().unwrap().join(f.unwrap()).to_string_lossy().to_string();
+        }
+        return String::default();
+    }
 }
 
 unsafe impl Send for AppUi {}
@@ -21,6 +144,7 @@ impl Default for AppUi {
             audio_device: Some(init_audio_device_default().unwrap()),
             player: None,
             media_path: String::default(),
+            no_scale: true,
             // stream_size_scale: 1.0,
             // seek_frac: 0.0,
         }
@@ -91,35 +215,71 @@ impl eframe::App for AppUi {
                         if let Some(buf) = rfd::FileDialog::new().add_filter("videos", &["mp4"]).pick_file() {
                             let f = buf.as_path().to_string_lossy().to_string();
                             self.media_path = f;
+                            if !self.media_path.is_empty() {
+                                let f = self.media_path.clone();
+                                // self.media_path = "".to_owned();
+                                match Player::new(ctx, &f.replace("\"", "")).and_then(|p| p.with_audio(&mut self.audio_device.as_mut().unwrap())) {
+                                    Ok(p) => {
+                                        self.player = Some(p);
+                                    }
+                                    Err(e) => {
+                                        log::error!("{}", e);
+                                    }
+                                }
+                            }
                         }
                     }
                     if ui.button("Close").clicked() {
                         self.player = None;
                     }
+
+                    ui.checkbox(&mut self.no_scale, "no scale");
+
+                    if !self.media_path.is_empty() {
+                        ui.label(self.media_path.clone());
+                        let mut file = String::default();
+                        if ui.button("Next file").clicked() {
+                            file = AppUi::next_file(&self.media_path);
+                        }
+                        if ui.button("Pre file").clicked() {
+                            file = AppUi::pre_file(&self.media_path);
+                        }
+
+                        if !file.is_empty() {
+                            self.media_path = file;
+                            let f = self.media_path.clone();
+                            match Player::new(ctx, &f.replace("\"", "")).and_then(|p| p.with_audio(&mut self.audio_device.as_mut().unwrap())) {
+                                Ok(p) => {
+                                    self.player = Some(p);
+                                }
+                                Err(e) => {
+                                    log::error!("{}", e);
+                                }
+                            }
+                        }
+                    }
+
                     ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
                 });
         }
-        if !self.media_path.is_empty() {
-            let f = self.media_path.clone();
-            self.media_path = "".to_owned();
-            match Player::new(ctx, &f.replace("\"", "")).and_then(|p| p.with_audio(&mut self.audio_device.as_mut().unwrap())) {
-                Ok(p) => {
-                    self.player = Some(p);
-                }
-                Err(e) => {
-                    log::error!("{}", e);
-                }
-            }
-        }
+
 
         egui::CentralPanel::default().frame(frame)
             .show(ctx, |ui| {
                 if let Some(player) = &mut self.player {
-                    let p = AppUi::compute_player_size(egui::Vec2::new(player.width as f32, player.height as f32),
-                                                       egui::Vec2::new(ui.min_rect().width(), ui.min_rect().height()));
+                    let p = {
+                        if self.no_scale {
+                            egui::Vec2::new(player.width as f32, player.height as f32)
+                        } else {
+                            AppUi::compute_player_size(egui::Vec2::new(player.width as f32, player.height as f32),
+                                                       egui::Vec2::new(ui.min_rect().width(), ui.min_rect().height()))
+                        }
+                    };
                     ui.centered_and_justified(|ui| {
                         player.ui(ui, [p.x, p.y]);
                     });
+
+                    AppUi::handle_key_player(ui, player);
                 }
 
                 let rect = {
@@ -135,7 +295,6 @@ impl eframe::App for AppUi {
                 if button.clicked() {
                     self.collapse = !self.collapse;
                 }
-
                 // dont use the following code, the t.response.rect.height() == ui.max_rect().height()
                 // let t = ui.with_layout(egui::Layout::right_to_left(egui::Align::Center),|ui|{
                 //     if ui.button(app_ui.collapse_str()).clicked(){
