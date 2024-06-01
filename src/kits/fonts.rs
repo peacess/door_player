f7089c0f3e279416c7ec4d73cce274bc3ca6d8f4
Peace <peacess@163.com>
2024-06-01 10:34:13 +0800

    create font file after get url ok

--- src/kits/fonts.rs
+++ src/kits/fonts.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::path::PathBuf;
+use std::{fs, mem};
 
 pub fn get_font() -> Result<PathBuf, anyhow::Error> {
     let file = "assets/fonts/文泉驿正黑.ttc";
@@ -11,26 +11,31 @@ pub fn get_font() -> Result<PathBuf, anyhow::Error> {
     // download url: https://wordshub.github.io/free-font/assets/font/%E4%B8%AD%E6%96%87/%E6%96%87%E6%B3%89%E9%A9%BF%E7%B3%BB%E5%88%97/%E6%96%87%E6%B3%89%E9%A9%BF%E6%AD%A3%E9%BB%91.ttc
     // see: https://github.com/orgs/community/discussions/42655#discussioncomment-5669289
 
-    let exe_path = {
-        let temp = std::env::current_exe()?;
-        temp.parent().expect("").to_owned()
-    };
+    let mut file_path = PathBuf::from(file);
+    if !file_path.exists() {
+        let exe_path = {
+            let temp = std::env::current_exe()?;
+            temp.parent().expect("").to_owned()
+        };
 
-    let full_file = exe_path.join(file);
-
-    if !full_file.exists() {
-        // check the path exist, if not, create it.
-        {
-            let p = full_file.parent().expect("");
-            if !p.exists() {
-                fs::create_dir_all(p)?;
+        let exe_path = exe_path.join(file);
+        if !exe_path.exists() {
+            {
+                let p = exe_path.parent().expect("");
+                if !p.exists() {
+                    fs::create_dir_all(p)?;
+                }
+            }
+            let mut response = reqwest::blocking::get(url)?;
+            let mut exe_file = fs::File::create(exe_path.clone())?;
+            if let Err(e) = response.copy_to(&mut exe_file) {
+                mem::drop(exe_file);
+                let _ = fs::remove_file(exe_path);
+                return Err(e.into());
             }
         }
-
-        let mut response = reqwest::blocking::get(url)?;
-        let mut file = fs::File::create(file)?;
-        response.copy_to(&mut file)?;
+        file_path = exe_path
     }
 
-    Ok(full_file)
+    Ok(file_path)
 }
